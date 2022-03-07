7552f3b307cf74fa9c9d8cadaedf7a14084ba2d8
Lamngoctrai <85330501+Lamngoctrai@users.noreply.github.com>
2022-03-07 20:54:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Chiếc web xàm thui",
+    introTitle: 'Hương hỏ!',
+    introDesc: `Chiếc web này cho XH ngày 8/3 đó :3`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Ngoc Trai dep trai dung hong XH 🥰',
+    desc: 'Quá đúng còn gì nữa =)))) ',
+    btnYes: 'Đúng òi XH còn xĩu',
+    btnNo: 'Khum',
+    question: 'Hong có chửi chó nha biết thế nào cũng chửi ',
+    btnReply: 'Gửi cho NT',
+    reply: 'Okiii aiu',
+    mess: 'Ơ thì mượn chiếc web này chúc XH 8/3 dui dẻ nhaaa',
+    messDesc: 'Hong có hoa vì không ai lại tặng hoa cho hoa cả .',
+    btnAccept: 'Vậy ó hỏ',
  
 }
